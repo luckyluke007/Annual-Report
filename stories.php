b4eb983f9b35221704c2b4c95e33ebf9518387b8
Trung Tran <trung@scgstudio.com>
2013-11-21 11:24:47 -0800

    Stories
    Stories winter

--- stories.php
+++ stories.php
@@ -9,31 +9,55 @@
 
 <h1>Stories</h1>
 <!-- STORIES -->
+<div id="stories-winter">WINTER STORIES</div>
+<div id="stories-container">
+<ul>
+<li><img src="images/profiles/meka.jpg" width="400" height="400" alt="Carson" />
+  <h3>Using her mind</h3>
+<p>Student Meka Williams's fierce determination to succeed—along with help from campus and private support programs—diverted her from a grim path.</p>
+<p>Read More »</p>
+</li>
+<li><img src="images/profiles/carson.jpg" width="400" height="400" alt="Carson" />
+  <h3>Way beyond a job</h3>
+<p>The unique and groundbreaking program has supported over 5,000 students since it started in 1986, equivalent to 5 percent of UCSC's total alumni base.</p>
+<p>Read More »</p>
+</li>
+</ul>
+<div class="clear"></div>
+</div>
+
+<!--FALL-->
+<div id="stories-fall">FALL STORIES</div>
 <div id="stories-container">
 <ul>
 <li><img src="images/profiles/carson.jpg" width="400" height="400" alt="Carson" />
   <h3>Gabe Zimmerman's memory lives on in scholarship</h3>
 <p>Student Carson Watts was the second winner of the Gabriel Zimmerman Memorial Scholarship, which supports students who are passionate about social issues and committed to public service.</p>
+<p>Read More »</p>
 </li>
 
 <li><img src="images/profiles/infant.jpg" width="400" height="400" alt="Carson" />
   <h3>Child's play = new research</h3>
 <p>A $165,000 grant to UCSC's Early Education Services has enabled the program to extend childcare hours for students' children and provide an observation room for developmental psychology research.</p>
+<p>Read More »</p>
 </li>
 
 <li><img src="images/profiles/karen.jpg" width="400" height="400" alt="Carson" />
   <h3>Giving back by paying forward</h3>
 <p>Karen Rhodes and her husband, fellow Slug Robert Weiner, have been consistent, reliable donors to UC Santa Cruz, providing valuable support for Cowell College, the History Department, scholarships, and more.</p>
+<p>Read More »</p>
 </li>
 
 <li><img src="images/profiles/josh.jpg" width="400" height="400" alt="Carson" />
   <h3>Beyond the comfort zone</h3>
 <p>With the help of an Irwin Project Grant, which funds student art projects on campus, art and psychology major Josh Katz launched a new trajectory of art-making.</p>
+<p>Read More »</p>
 </li>
 
 <li><img src="images/profiles/donka.jpg" width="400" height="400" alt="Carson" />
   <h3>Honoring outstanding teaching in the humanities</h3>
 <p>The Dizikes teaching award—named for professor emeritus of American studies John Dizikes—celebrates the Humanities faculty's commitment to excellence in teaching and its impact on undergraduate students.</p>
+<p>Read More »</p>
 </li>
 </ul>
 <div class="clear"></div>
