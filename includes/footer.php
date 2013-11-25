5fcb7c1b760583b831231250328533c5bc09f85a
Trung Tran <trung@scgstudio.com>
2013-11-25 10:05:00 -0800

    Removed
    and Updating footers

--- includes/footer.php
+++ includes/footer.php
@@ -7,10 +7,10 @@
         </div>
         <div class="right">
         <span class="social-media">
- <a href="https://www.facebook.com/ucsantacruz"><img src="images/facebook.gif" width="82" height="36" alt="UCSC Facebook" border="0"></a>
- <a href="http://twitter.com/ucsc"><img src="images/twitter.gif" width="82" height="36" alt="UCSC Twitter" border="0"></a>
- <a href="http://www.youtube.com/ucsantacruz"><img src="images/youtube.gif" width="82" height="36" alt="UCSC Youtube" border="0"></a>
- <a href="http://www.linkedin.com/groups?home=&gid=102708"><img src="images/linkedin.gif" width="82" height="36" alt="UCSC LinkedIn" border="0"></a>
+ <a href="https://www.facebook.com/ucsantacruz"><img src="images/facebook.png" width="82" height="36" alt="UCSC Facebook" border="0"></a>
+ <a href="http://twitter.com/ucsc"><img src="images/twitter.png" width="82" height="36" alt="UCSC Twitter" border="0"></a>
+ <a href="http://www.youtube.com/ucsantacruz"><img src="images/youtube.png" width="82" height="36" alt="UCSC Youtube" border="0"></a>
+ <a href="http://www.linkedin.com/groups?home=&gid=102708"><img src="images/linkedin.png" width="82" height="36" alt="UCSC LinkedIn" border="0"></a>
  </span>
         </div>
     </div>
--- stories.php
+++ stories.php
@@ -5,6 +5,7 @@
 
 <title>Stories | 2012-13 Annual Report on Philanthropy</title>
 <?php include("includes/header.php"); ?>
+<link rel="stylesheet" type="text/css" href="css/buttons.css" />
 <div class="content">
 
 <h1>Stories</h1>
@@ -12,35 +13,31 @@
 <div id="stories-winter">WINTER STORIES</div>
 <div id="stories-container">
 <ul>
-<li><img src="images/profiles/meka.jpg" width="400" height="400" alt="Carson" />
+<li><img src="images/profiles/give.jpg" width="400" height="400" alt="Give. Don't Give In - Thumb" />
+<h3>Give. Don't Give In</h3>
+<p>In these pages, you will read about gifts toward the Campaign for UC Santa Cruz, which will support our culture of looking past previously accepted boundaries.</p>
+<p><a href="#" class="a_button_one">Read More »</a></p>
+</li>
+<li><img src="images/profiles/meka.jpg" width="400" height="400" alt="Meka Williams" />
   <h3>Choosing a different path: meet Meka Williams</h3>
 <p>Student Meka Williams's fierce determination to succeed—along with help from campus and private support programs—diverted her from a grim path.</p>
-<p>Read More »</p>
+<p><a href="#" class="a_button_one">Read More »</a></p>
 </li>
 <li><img src="images/profiles/konopelski.jpg" width="400" height="400" alt="Joe Konopelski and Nancy Cox-Konopelski" />
   <h3>Way beyond a job</h3>
 <p>The unique and groundbreaking program has supported over 5,000 students since it started in 1986, equivalent to 5 percent of UCSC's total alumni base.</p>
-<p>Read More »</p>
+<p><a href="#" class="a_button_one">Read More »</a></p>
 </li>
 <li><img src="images/profiles/rebeles.jpg" width="400" height="400" alt="Rebeles" />
   <h3>Connecting science and justice</h3>
 <p>UCSC's innovative Science and Justice Training Program trains graduate students to think hard about the effects and impacts of their research on society, offering a pathway for science and engineering to connect to social issues and public concerns.</p>
-<p>Read More »</p>
+<p><a href="#" class="a_button_one">Read More »</a></p>
 </li>
-<li><img src="images/profiles/david-haussler.jpg" width="400" height="400" alt="David Haussler" />
-  <h3>Nobody said we couldn't</h3>
-<p>"I was able to ask the question, what is the molecular basis for cancer? And how can we have a precision treatment? Nobody said I couldn't do that," said David Haussler, director of UCSC's Center for Biomolecular Science and Engineering. "And we built the biggest cancer database in the world—even though we don't even have a med school. Nobody said we couldn't assemble the human genome. So we did!"</p>
-<p>Read More »</p>
-</li>
+
 <li><img src="images/profiles/pehl.jpg" width="400" height="400" alt="Dorothy Everett" />
   <h3>Honoring a 'quietly incredible woman'</h3>
 <p>Mark Headley (Stevenson '83, politics) and his wife, Christina Pehl, decided to make a gift establishing a new endowed chair at UCSC, they wanted to commemorate an extraordinary woman who worked tirelessly for three University of California presidents.</p>
-<p>Read More »</p>
-</li>
-<li><img src="images/profiles/josh-stuart.jpg" width="400" height="400" alt="Josh Stuart" />
-  <h3>Sparking a relationship between art and science</h3>
-<p>Josh Stuart, professor of biomolecular engineering at UCSC, helped organize the Pan-Cancer Initiative, an international effort across campus using data sciences that could help bring us closer to a time when doctors treat cancer based on patients' genetic profiles—potentially bringing about a massive shift in thinking about the way cancer treatment works. </p>
-<p>Read More »</p>
+<p><a href="#" class="a_button_one">Read More »</a></p>
 </li>
 </ul>
 <div class="clear"></div>
@@ -53,31 +50,31 @@
 <li><img src="images/profiles/carson.jpg" width="400" height="400" alt="Carson" />
   <h3>Gabe Zimmerman's memory lives on in scholarship</h3>
 <p>Student Carson Watts was the second winner of the Gabriel Zimmerman Memorial Scholarship, which supports students who are passionate about social issues and committed to public service.</p>
-<p><a href="articles/watts-carson.php">Read More »</a></p>
+<p><a href="articles/watts-carson.php" class="a_button_one">Read More »</a></p>
 </li>
 
 <li><img src="images/profiles/infant.jpg" width="400" height="400" alt="Carson" />
   <h3>Child's play = new research</h3>
 <p>A $165,000 grant to UCSC's Early Education Services has enabled the program to extend childcare hours for students' children and provide an observation room for developmental psychology research.</p>
-<p><a href="articles/early-education.php">Read More »</a></p>
+<p><a href="articles/early-education.php" class="a_button_one">Read More »</a></p>
 </li>
 
 <li><img src="images/profiles/karen.jpg" width="400" height="400" alt="Carson" />
   <h3>Giving back by paying forward</h3>
 <p>Karen Rhodes and her husband, fellow Slug Robert Weiner, have been consistent, reliable donors to UC Santa Cruz, providing valuable support for Cowell College, the History Department, scholarships, and more.</p>
-<p><a href="articles/karen-rhodes.php">Read More »</a></p>
+<p><a href="articles/karen-rhodes.php" class="a_button_one">Read More »</a></p>
 </li>
 
 <li><img src="images/profiles/josh.jpg" width="400" height="400" alt="Carson" />
   <h3>Beyond the comfort zone</h3>
 <p>With the help of an Irwin Project Grant, which funds student art projects on campus, art and psychology major Josh Katz launched a new trajectory of art-making.</p>
-<p><a href="articles/josh-katz.php">Read More »</a></p>
+<p><a href="articles/josh-katz.php" class="a_button_one">Read More »</a></p>
 </li>
 
 <li><img src="images/profiles/donka.jpg" width="400" height="400" alt="Carson" />
   <h3>Honoring outstanding teaching in the humanities</h3>
 <p>The Dizikes teaching award—named for professor emeritus of American studies John Dizikes—celebrates the Humanities faculty's commitment to excellence in teaching and its impact on undergraduate students.</p>
-<p><a href="articles/dizikes.php">Read More »</a></p>
+<p><a href="articles/dizikes.php" class="a_button_one">Read More »</a></p>
 </li>
 </ul>
 <div class="clear"></div>
