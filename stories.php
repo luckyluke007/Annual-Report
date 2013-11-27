8544068c8fa11e68cac69a0c7fc972f68f792c19
Trung Tran <trung@scgstudio.com>
2013-11-27 09:47:33 -0800

    stories updated with links for new stories

--- stories.php
+++ stories.php
@@ -15,29 +15,29 @@
 <ul>
 <li><img src="images/profiles/give.jpg" width="400" height="400" alt="Give. Don't Give In - Thumb" />
 <h3>Give. Don't Give In</h3>
-<p>In these pages, you will read about gifts toward the Campaign for UC Santa Cruz, which will support our culture of looking past previously accepted boundaries.</p>
-<p><a href="#" class="a_button_one">Read More »</a></p>
+<p>UC Santa Cruz is the boldest experiment in the history of public higher education. Our very founding in 1965 was a revolt against the educational status quo.</p>
+<p><a href="articles/give-112013.php" class="a_button_one">Read More »</a></p>
 </li>
 <li><img src="images/profiles/meka.jpg" width="400" height="400" alt="Meka Williams" />
   <h3>Choosing a different path: meet Meka Williams</h3>
 <p>Student Meka Williams's fierce determination to succeed—along with help from campus and private support programs—diverted her from a grim path.</p>
-<p><a href="#" class="a_button_one">Read More »</a></p>
+<p><a href="articles/meetmeka-112013.php" class="a_button_one">Read More »</a></p>
 </li>
 <li><img src="images/profiles/konopelski.jpg" width="400" height="400" alt="Joe Konopelski and Nancy Cox-Konopelski" />
   <h3>Way beyond a job</h3>
 <p>The unique and groundbreaking program has supported over 5,000 students since it started in 1986, equivalent to 5 percent of UCSC's total alumni base.</p>
-<p><a href="#" class="a_button_one">Read More »</a></p>
+<p><a href="articles/waybeyondajob-112013.php" class="a_button_one">Read More »</a></p>
 </li>
 <li><img src="images/profiles/rebeles.jpg" width="400" height="400" alt="Rebeles" />
   <h3>Connecting science and justice</h3>
 <p>UCSC's innovative Science and Justice Training Program trains graduate students to think hard about the effects and impacts of their research on society, offering a pathway for science and engineering to connect to social issues and public concerns.</p>
-<p><a href="#" class="a_button_one">Read More »</a></p>
+<p><a href="articles/connecting-112013.php" class="a_button_one">Read More »</a></p>
 </li>
 
 <li><img src="images/profiles/pehl.jpg" width="400" height="400" alt="Dorothy Everett" />
   <h3>Honoring a 'quietly incredible woman'</h3>
 <p>Mark Headley (Stevenson '83, politics) and his wife, Christina Pehl, decided to make a gift establishing a new endowed chair at UCSC, they wanted to commemorate an extraordinary woman who worked tirelessly for three University of California presidents.</p>
-<p><a href="#" class="a_button_one">Read More »</a></p>
+<p><a href="articles/incrediblewoman-112013.php" class="a_button_one">Read More »</a></p>
 </li>
 </ul>
 <div class="clear"></div>
